c039ed806b28f953e448d3b7417cdcdcab080c12
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-19 23:18:34 +0545

    Create response in second activity

--- TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
@@ -1,21 +1,24 @@
-package com.example.twoactivities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class activity_second extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(com.example.android.twoactivities.MainActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
