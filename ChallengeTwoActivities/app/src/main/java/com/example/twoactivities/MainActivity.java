13b7423b7375967519c8772f205dcf2692c200ed
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-19 23:46:50 +0545

    getting message any button clicked

--- ChallengeTwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ ChallengeTwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -8,47 +8,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE
             = "com.example.android.twoactivities.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
+    public void showText(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
 
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
+        switch (view.getId()){
+            case R.id.button_one:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_one)); startActivity(intent);
+                break;
+            case R.id.button_two:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_two)); startActivity(intent);
+                break;
+            case R.id.button_three:
+                intent.putExtra(EXTRA_MESSAGE, getString(R.string.display_three)); startActivity(intent);
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "Error Detected", Toast.LENGTH_LONG).show();
+                break;
         }
     }
 }
